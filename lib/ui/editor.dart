3224e39b674a4cd06a1b05e9b7dc33f640e8b980
Masaki Hikosaka <masaki_hikosaka@outlook.com>
2019-05-01 14:22:48 +0900

    migrate to stateless widget

--- lib/ui/editor.dart
+++ lib/ui/editor.dart
@@ -3,69 +3,71 @@ import 'package:flutter_markdown/flutter_markdown.dart';
 import 'package:bloc_provider/bloc_provider.dart';
 import 'package:markdown_editor/bloc/editor_bloc.dart';
 
-class Editor extends StatefulWidget {
-  @override
-  _EditorState createState() => _EditorState();
-}
-
-class _EditorState extends State<Editor> with SingleTickerProviderStateMixin {
-  TabController _tabController;
+class Editor extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final bloc = BlocProvider.of<EditorBloc>(context);
 
     const TextStyle style = TextStyle(
-      color: Colors.black,
+      color: Colors.white,
     );
 
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.white,
-        title: Text(
-          "Markdown Editor",
-          style: style,
-        ),
-        bottom: TabBar(controller: _tabController, tabs: [
-          Tab(
-            child: Text('Editing', style: style),
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text(
+            "Markdown Editor",
+            style: style,
           ),
-          Tab(
-            child: Text('Preview', style: style),
-          )
-        ]),
-      ),
-      body: TabBarView(
-        controller: _tabController,
-        children: <Widget>[
-          Container(
-            margin: EdgeInsets.all(20),
-            child: TextField(
-              maxLines: null,
-              onChanged: (value) => {bloc.content.add(value)},
-              decoration: InputDecoration(
-                border: InputBorder.none,
-                hintText: 'INPUT TEXT',
+          bottom: TabBar(
+            tabs: [
+              Tab(
+                child: Column(
+                  children: <Widget>[
+                    Icon(Icons.edit),
+                    Text('Editing', style: style),
+                  ],
+                ),
+              ),
+              Tab(
+                child: Column(
+                  children: <Widget>[
+                    Icon(Icons.remove_red_eye),
+                    Text('Preview', style: style),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+        body: TabBarView(
+          children: <Widget>[
+            Container(
+              margin: EdgeInsets.all(20),
+              child: TextField(
+                maxLines: null,
+                onChanged: (value) => {bloc.content.add(value)},
+                decoration: InputDecoration(
+                  border: InputBorder.none,
+                  hintText: 'INPUT TEXT',
+                ),
               ),
             ),
-          ),
-          Container(
-            margin: EdgeInsets.all(20),
-            child: StreamBuilder<String>(
-              stream: bloc.content,
-              initialData: bloc.content.value,
-              builder: (context, snap) {
-                return MarkdownBody(data: snap.data);
-              },
+            Container(
+              margin: EdgeInsets.all(20),
+              child: StreamBuilder<String>(
+                stream: bloc.content,
+                initialData: bloc.content.value,
+                builder: (context, snap) {
+                  return MarkdownBody(data: snap.data);
+                },
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    _tabController = TabController(length: 2, vsync: this);
   }
 }
